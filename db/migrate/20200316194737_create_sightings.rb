c253aebb3740be8d879e8897778d37925a3a4ad3
Noah Scala <nwscala@gmail.com>
2020-03-16 17:06:20 -0400

    fixed sightings migration

--- db/migrate/20200316194737_create_sightings.rb
+++ db/migrate/20200316194737_create_sightings.rb
@@ -1,8 +1,8 @@
 class CreateSightings < ActiveRecord::Migration[6.0]
   def change
     create_table :sightings do |t|
-      t.birdwatcher :belongs_to
-      t.bird :belongs_to
+      t.belongs_to :birdwatcher, foreign_key: true
+      t.belongs_to :bird, foreign_key: true
       t.string :location
       t.string :time_of_day
       t.string :time_of_year
