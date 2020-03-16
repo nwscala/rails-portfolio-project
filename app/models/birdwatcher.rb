418d92e954b95ebdab79984025416a880ab0a3e0
Noah Scala <nwscala@gmail.com>
2020-03-16 17:08:42 -0400

    added blank views & scope method to birdwatchers

--- app/models/birdwatcher.rb
+++ app/models/birdwatcher.rb
@@ -1,4 +1,8 @@
 class Birdwatcher < ApplicationRecord
     has_many :sightings
     has_many :birds, through: :sightings
+
+    def self.ornithologist
+        where(ornithologist: true)
+    end
 end
