e11d7dc2aea3b7a179707da2cef503eb55962053
Noah Scala <nwscala@gmail.com>
2020-03-16 17:07:15 -0400

    added home route under static controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :sightings
   resources :birds
   resources :birdwatchers
-  # get 'static/home'
+  root to: 'static#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
