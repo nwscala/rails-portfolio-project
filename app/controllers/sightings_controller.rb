3075364cc9880d011f2c4c2ad1fbc11b51e5fad8
Noah Scala <nwscala@gmail.com>
2020-03-18 15:51:52 -0400

    build sightings functionality and views

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,31 +1,78 @@
 class SightingsController < ApplicationController
-    def index
-        @birdwatchers = Birdwatcher.all
+    before_action :birdwatcher_check
+    before_action :set_sighting, except: [:index, :new, :create]
 
-        @sightings = Sighting.all
+    def index
+        if params[:birdwatcher_id]
+            @sightings = @birdwatcher.sightings
+        else
+            @sightings = Sighting.all
+        end
     end
     
     def new
-
+        @sighting = current_user.sightings.build
     end
 
     def create
+        @sighting = current_user.sightings.build(sighting_params)
+        if @sighting.save
+            redirect_to sightings_path
+        else
+            render :new
+        end
+    end
+
+    def show 
 
     end
 
     def edit
-
+        if current_user != @birdwatcher
+            flash[:notice] = "You cannot edit someone else's sighting!"
+            redirect_to root_path
+        end
     end
 
     def update
-
+        if @sighting.update(sighting_params)
+            redirect_to birdwatcher_sighting_path(@birdwatcher, @sighting)
+        else    
+            render :edit
+        end
     end
 
     def delete
-
+        if current_user == @birdwatcher
+            @sighting.destroy
+            redirect_to sightings_path
+        else
+            flash[:notice] = "You cannot delete someone else's sighting!"
+            redirect_to root_path
+        end
     end
 
     private
 
-    
+        def sighting_params
+            params.require(:sighting).permit(:bird_id, :location, :time_of_day, :time_of_year)
+        end
+
+        def birdwatcher_check
+            if params[:birdwatcher_id]
+                @birdwatcher = Birdwatcher.find_by_id(params[:birdwatcher_id])
+                if !@birdwatcher
+                    flash[:notice] = "It seems like that birdwatcher isn't in our database."
+                    redirect_to root_path
+                end
+            end
+        end
+
+        def set_sighting
+            @sighting = Sighting.find_by_id(params[:id])
+            if !@sighting
+                flash[:notice] = "It seems like there isn't a sighting with that id in our database."
+                redirect_to root_path
+            end
+        end
 end
--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -1,4 +1,7 @@
 class Sighting < ApplicationRecord
     belongs_to :bird
     belongs_to :birdwatcher
+    validates :location, presence: true
+    validates :time_of_day, presence: true
+    validates :time_of_year, presence: true
 end
